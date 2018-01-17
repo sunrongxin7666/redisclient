5574129b65ca6c2ceb94672d4419409879dec445
sunrongxin7666 <sunrongxin7666@163.com>
2018-01-17 16:29:32 +0800

    Initial commit

---
+++ src/main/java/srx/awesome/code/redisclient/RedisClientDemo.java
@@ -0,0 +1,50 @@
+package srx.awesome.code.redisclient;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisClientDemo {
+    public static void main(String[] args) {
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //set the data in redis string
+        jedis.set("tutorial-name", "Redis tutorial");
+        // Get the stored data and print it
+        System.out.println("Stored string in redis:: "+ jedis.get("tutorial-name"));
+    }
+
+    @Test
+    public void demo1(){
+        //1. Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //2. set the data in redis string
+        jedis.set("username", "Roxin");
+        //3. Get the stored data and print it
+        System.out.println("Stored string in redis:: "+ jedis.get("username"));
+        //4. Close the Redis connection;
+        jedis.close();
+    }
+
+    @Test
+    public void demo2(){
+        //连接池设定
+        JedisPoolConfig config = new JedisPoolConfig();
+        //设定最大连接数
+        config.setMaxTotal(30);
+        //设置最大空闲连接数
+        config.setMaxIdle(10);
+        //创建连接池
+        JedisPool jedisPool = new JedisPool(config, "127.0.0.1");
+        //获得服务资源
+        Jedis jedis = jedisPool.getResource();
+        jedis.set("username", "Roxin By Jedis Pool");
+        System.out.println(jedis.get("username"));
+
+        jedis.close();
+        jedisPool.close();
+    }
+}
